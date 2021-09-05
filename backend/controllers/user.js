da9355904f85021ef1514e9c296e4a73cc0ac070
Saleh Mostafa <mostafas@student.unimelb.edu.au>
2021-09-05 12:29:36 +1000

    added the functionality to the user file in the routes and controllers modules

---
+++ backend/controllers/user.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import User from '../models/user.js';
+
+const router = express.Router();
+
+// Create user
+export const createUser = async (req, res) => {
+    const {email, password, nameFirst, nameLast, type, clients, orders,
+    contracts, reports} = req.body;
+
+    const newUser = new User({email, password, nameFirst, nameLast, type, 
+        clients, orders, contracts, reports})
+
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    }
+    catch (error) {
+        res.status(409).json({message: error.message});
+    }
+}
+
+// Get user
+export const getUser = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id);
+        
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+// Update user
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const {email, password, nameFirst, nameLast, type, clients, orders,
+        contracts, reports, _id: id}; 
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) 
+        return res.status(404).send(`No user with id: ${id}`);
+
+    const updatedUser = {email, password, nameFirst, nameLast, type, clients, orders,
+        contracts, reports} = req.body;
+
+    await User.findByIdAndUpdate(id, updatedUser, { new: true });
+
+    res.json(updatedUser);
+}
+
+// Delete user
+export const deleteUser = async (req, res) => {
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) 
+        return res.status(404).send(`No user with id: ${id}`);
+    
+    await User.findByIdAndRemove(id);
+
+    res.json({message: "User deleted successfully."});
+}
+
+// Get all users
+export const getAllUsers = async (req, res) => { 
+    try {
+        const allUsers = await User.find();
+                
+        res.status(200).json(allUsers);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
