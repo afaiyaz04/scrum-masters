ed07e7ed6a9aa74c7a7af02f048bbf950c47a2f9
Emmanuel <emmanuel.pinca098@gmail.com>
2021-09-05 10:38:03 +1000

    added db connection

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin@scrummasters-it-project.gtsnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(
+    CONNECTION_URL, 
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => app.listen(PORT, () => console.log('MongoDB connected')))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
