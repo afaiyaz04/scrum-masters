571cd4e3f78add4b6afdb8e343007e19c9af2de9
Emmanuel <emmanuel.pinca098@gmail.com>
2021-09-10 11:35:28 +1000

    removed index.js from models

--- backend/index.js
+++ backend/index.js
@@ -21,3 +21,31 @@ mongoose.connect(
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
+
+// // Set up Mongoose
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.MONGODB_URI, {
+// 	useNewUrlParser: true,
+// 	useCreateIndex: true,
+// 	useUnifiedTopology: true,
+// 	useFindAndModify: false,
+// 	dbName: "Add DataBase Name here",
+// });
+
+// const db = mongoose.connection;
+// db.on("error", (err) => {
+// 	console.error(err);
+// 	process.exit(1);
+// });
+
+// db.once("open", async () => {
+// 	console.log("Mongo connection started on " + db.host + ":" + db.port);
+// });
+
+// // Require individual models
+// require("./product");
+// require("./order");
+// require("./client");
+// require("./user");
