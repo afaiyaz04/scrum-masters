8f8f45a65504d9ec2f6b0cfa270b7643089a228b
Alex <alexandersiohsavage@gmail.com>
2021-09-15 17:45:28 +1000

    add routes to view all a user's orders and add order to user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,14 @@
 import express from 'express';
 
-import { createUser, getUser, updateUser, deleteUser, getAllUsers } from '../controllers/user.js';
+import { 
+    createUser, 
+    getUser, 
+    updateUser, 
+    deleteUser, 
+    getAllUsers,
+    getUserOrders,
+    addUserOrder
+} from '../controllers/user.js';
 
 const router = express.Router();
 
@@ -9,6 +17,8 @@ router.post('/', createUser);
 router.get('/:id', getUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
+router.get('/:id/orders', getUserOrders);
+router.post('/:id/orders', addUserOrder);
 
 export default router;
 
