7a0b5eee16a38abb0e09f83119e1768f74c9b1fa
Alex <alexandersiohsavage@gmail.com>
2021-09-19 11:30:04 +1000

    added routes for getting clients and adding a client

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,18 +7,25 @@ import {
     deleteUser, 
     getAllUsers,
     getUserOrders,
-    addUserOrder
+    addUserOrder,
+    getUserClients,
+    addUserClient
 } from '../controllers/user.js';
 
 const router = express.Router();
-
+// CRUD
 router.get('/', getAllUsers);
 router.post('/', createUser);
 router.get('/:id', getUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
+
+// Adding an order to a user
 router.get('/:id/orders', getUserOrders);
 router.post('/:id/orders', addUserOrder);
 
+// Adding a client to a user
+router.get('/:id/clients', getUserClients);
+router.post('/:id/clients', addUserClient);
 export default router;
 
