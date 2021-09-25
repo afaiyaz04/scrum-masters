5ae0bd207317c6fb2eec076edab9e20e3a8a7a34
Md Reaj Uddin Rabbi <mdreajuddinrabbi@gmail.com>
2021-09-26 08:38:56 +1000

    Added buttons to edit/delete entities

--- frontend/my-app/src/components/ItemDetails.js
+++ frontend/my-app/src/components/ItemDetails.js
@@ -6,11 +6,13 @@ function ItemDetails(props) {
     const details = props.details;
     return (
         <div className='contents-right'>
-            <h2>{Item} details</h2>
+            <h2>{Item} Details</h2>
             <div className="list-item-details">
-                <h3>Type: {type}</h3>
-                <h3>Details: {details}</h3>
+                <h3>Type: {type} </h3> 
+                <h3>Details: {details} </h3>
             </div>
+            <button>Edit</button>
+            <button>Delete</button>
         </div>
     )
 }
