3a487665aa75000efcb883bb3bb8eee52994d290
Alex <alexandersiohsavage@gmail.com>
2021-09-27 12:17:34 +1000

    added handling for when a report cannot be generated for a user

--- backend/controllers/report.js
+++ backend/controllers/report.js
@@ -9,74 +9,79 @@ const router = express.Router();
 
 export const viewReport = async (req, res) => {
     const { id } = req.params;
-    const report = await generateReport(id, res);
-    res.json(report);
+    try {
+        const user = await User.findById(id);
+        const report = await generateReport(user);
+        return res.json(report);
+    } catch (error) {
+        return res.status(404).json({message: error.message});
+    }
 };
 
 export const viewAllReports = async (req, res) => {
-    const allUsers = await User.find();
-    const allReports = [];
-    for (var i = 0; i < allUsers.length; i++){
-        const userReport = await generateReport(allUsers[i].id, res);
-        allReports.push(userReport);
+    try {
+        const allUsers = await User.find();
+        const allReports = [];
+        for (var i = 0; i < allUsers.length; i++){
+            const userReport = await generateReport(allUsers[i]);
+            allReports.push(userReport);
+        }
+        res.json(allReports);
+    } catch (error) {
+        return res.status(404).json({message: error.message});
     }
-    res.json(allReports);
 
 };
 
-async function generateReport(userId, res) {
-    try {
-        const user = await User.findById(userId);
-        if (user == null) {
-            return res.status(404).send(`No user with id: ${userId}`);
+async function generateReport(user) {
+    const name = user.nameFirst + " " + user.nameLast;
+
+    // number of total clients
+    const totalClients = user.clients.length;
+    const totalOrders = user.orders.length;
+
+    // aggregate order properties
+    const orderStatus = {}
+    var totalRevenue = 0;
+    for (var i = 0; i < user.orders.length; i++) {
+        var orderId = user.orders[i];
+        var order = await Order.findById(orderId);
+        
+        if (order == null) {
+            return { }
         }
-        const name = user.nameFirst + " " + user.nameLast;
-
-        // number of total clients
-        const totalClients = user.clients.length;
-        const totalOrders = user.orders.length;
-
-        // aggregate order properties
-        const orderStatus = {}
-        var totalRevenue = 0;
-        for (var i = 0; i < user.orders.length; i++) {
-            var orderId = user.orders[i];
-            var order = await Order.findById(orderId);
-            
-            // count orders by status
-            var currStatus = order.status;
-            if (!(orderStatus.hasOwnProperty(currStatus))) {
-                orderStatus[currStatus] = 1;
-            }
-            else { 
-                orderStatus[currStatus] += 1;
-            }
-            
-            // get revenue
-            for (var j = 0; j < order.lineProducts.length; j++) {
-                var quantity = order.lineProducts[j].quantity;
-                var product = await Product.findById(
-                    order.lineProducts[j].productId
-                );
-                var productPrice = product.price;
-                totalRevenue += (quantity * productPrice)
-            }
-            var orderFee = order.totalFee;
-            totalRevenue += orderFee;
+        // count orders by status
+        var currStatus = order.status;
+        if (!(orderStatus.hasOwnProperty(currStatus))) {
+            orderStatus[currStatus] = 1;
+        }
+        else { 
+            orderStatus[currStatus] += 1;
         }
         
-        return { 
-            name: name, 
-            orderStatus: orderStatus, 
-            totalOrders: totalOrders,
-            totalClients: totalClients, 
-            totalRevenue: totalRevenue
-        };
-
-    } catch (error) {
-        res.status(404).json({ message: error.message});
+        // get revenue
+        for (var j = 0; j < order.lineProducts.length; j++) {
+            var quantity = order.lineProducts[j].quantity;
+            var product = await Product.findById(
+                order.lineProducts[j].productId
+            );
+            if (product == null) {
+                return { }
+            }
+            var productPrice = product.price;
+            totalRevenue += (quantity * productPrice)
+        }
+        var orderFee = order.totalFee;
+        totalRevenue += orderFee;
     }
     
+    return { 
+        name: name, 
+        orderStatus: orderStatus, 
+        totalOrders: totalOrders,
+        totalClients: totalClients, 
+        totalRevenue: totalRevenue
+    };
 };
 
 export default router;
