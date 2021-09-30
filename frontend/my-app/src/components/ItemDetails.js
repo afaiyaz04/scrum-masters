5f622cdecc485b1d20bbdc1e35329022823ae09a
Emmanuel <emmanuel.pinca098@gmail.com>
2021-09-30 13:22:16 +1000

    added ant-design components and contact functionality

--- frontend/my-app/src/components/ItemDetails.js
+++ frontend/my-app/src/components/ItemDetails.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Input } from 'antd';
+import axios from 'axios';
+
+const updateHandler = (path, newItem) => {
+    axios.patch(path, newItem)
+}
 
 function ItemDetails(props) {
     const Item = props.item;
-    const type = props.type;
-    const details = props.details;
+    const path = props.path;
+    const [details, setDetails] = useState(props.details);
 
     return (
         <div className='contents-right'>
             <h2>{Item} Details</h2>
             <div className="list-item-details">
-                <h3>Type: {type} </h3> 
-                <h3>Details: {details} </h3>
+                { Object.keys(details).map(key => (<Input placeholder={ details[key] } onChange={(e) => setDetails({ ...details, key: e.target.value })} />)) }
             </div>
             <div className='edit-btns'>
-                <button>Edit</button>
+                <button onClick={() => alert(`${details['First Name']}`)}>Update</button>
                 <button>Delete</button>
             </div>
         </div>
--- frontend/my-app/src/pages/Contacts.js
+++ frontend/my-app/src/pages/Contacts.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from 'react';
 import Sidebar from "../components/sideBar/Sidebar";
 import Header from '../components/Header';
-import ContactListItem from '../components/ContactListItem';
-import ItemDetails from '../components/ItemDetails';
+import { CgProfile, CgMenu } from "react-icons/cg";
 import axios from 'axios';
+import { List, Input } from 'antd';
+import 'antd/dist/antd.css';
 
 export default class Contacts extends React.Component{
     state = {
-        contacts: []
+        contacts: [],
+
+        id: '',
+        nameFirst: '',
+        nameLast: '',
+        title: '',
+        company: '',
+        email: '',
+        phoneNumber: '',
+        address: ''
     }
 
     componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        axios.get(`http://localhost:5000/user/614180facb6259ce3427029f/clients`)
+            .then(res => {
+                console.log(res)
+                this.setState({contacts: res.data});
+            })
+    }
+
+    updateHandler = (newItem) => {
+        axios.patch(`http://localhost:5000/client/${newItem.id}`, newItem)
+        .then(res => {
+            console.log(res)
+            this.setState({
+                nameFirst: res.nameFirst,
+                nameLast: res.nameLast,
+                title: res.title,
+                company: res.company,
+                email: res.email,
+                phoneNumber: res.phoneNumber,
+                address: res.address
+            });
+        })
+        axios.get(`http://localhost:5000/user/614180facb6259ce3427029f/clients`)
             .then(res => {
                 console.log(res)
                 this.setState({contacts: res.data});
@@ -28,11 +59,52 @@ export default class Contacts extends React.Component{
                 <div className='contents'>
                     <div className='contents-left'>
                         <h3>Name</h3>
-                        <ul>
-                            {this.state.contacts.map(contacts => <ContactListItem name={contacts.name} email={contacts.email}></ContactListItem>)}
-                        </ul>
+                        <List
+                            itemLayout='horizontal'
+                            dataSource={this.state.contacts}
+                            renderItem={item => (
+                                <List.Item
+                                    key={item.id}
+                                    actions={
+                                        [<CgMenu
+                                            onClick={() => this.setState({
+                                                id: item._id,
+                                                nameFirst: item.nameFirst,
+                                                nameLast: item.nameLast,
+                                                title: item.title,
+                                                company: item.company,
+                                                email: item.email,
+                                                phoneNumber: item.phoneNumber,
+                                                address: item.address
+                                            })}
+                                        />]
+                                    }
+                                >
+                                    <List.Item.Meta
+                                        title={ `${item.nameFirst} ${item.nameLast}` }
+                                        description={item.email}
+                                        avatar={<CgProfile/>}
+                                    />
+                                </List.Item>
+                            )}
+                        />
                     </div>
-                    <ItemDetails item='Contact' type='Contact' details='Ahbab 04575757223 ScrumMaster'></ItemDetails>
+                    <div className='contents-right'>
+                        <h2>Contact Details</h2>
+                        <div className="list-item-details">
+                            <Input placeholder={ this.state.nameFirst } onChange={(e) => this.setState({ nameFirst: e.target.value })} />
+                            <Input placeholder={ this.state.nameLast } onChange={(e) => this.setState({ nameLast: e.target.value })} />
+                            <Input placeholder={ this.state.title } onChange={(e) => this.setState({ title: e.target.value })} />
+                            <Input placeholder={ this.state.company } onChange={(e) => this.setState({ company: e.target.value })} />
+                            <Input placeholder={ this.state.email } onChange={(e) => this.setState({ email: e.target.value })} />
+                            <Input placeholder={ this.state.phoneNumber } onChange={(e) => this.setState({ phoneNumber: e.target.value })} />
+                            <Input placeholder={ this.state.address } onChange={(e) => this.setState({ address: e.target.value })} />
+                        </div>
+                        <div className='edit-btns'>
+                            <button onClick={() => this.updateHandler(this.state)}>Update</button>
+                            <button>Delete</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
