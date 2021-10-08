7025706eac872a6fec4f951101391b56b634eba5
Emmanuel <emmanuel.pinca098@gmail.com>
2021-10-08 17:48:28 +1100

    implemented sign-out button

--- frontend/my-app/src/components/buttons/Signout.js
+++ frontend/my-app/src/components/buttons/Signout.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './Signout.css'
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { SIGN_OUT } from '../../redux/User/user.types';
 
-class Signout extends React.Component{
-	nextPath(path){
-		this.props.history.push(path);
+function Signout() {
+	const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+	const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')));
+	const history = useHistory();
+	const dispatch = useDispatch();
+
+	const signout = () => {
+		dispatch({ type:  SIGN_OUT });
+		history.push('/');
+		setUser(null);
+		setUserData(null);
 	}
 
-	render(){
-		return (
-			<button className="signout-btn" onClick={() => {this.nextPath('/profile')}}>
-                Sign Out
-			</button>
-		);
-	}
+	return (
+		<button className="signout-btn" onClick={signout}>
+			Sign Out
+		</button>
+	);
 }
 
 export default withRouter(Signout);
--- frontend/my-app/src/redux/User/user.types.js
+++ frontend/my-app/src/redux/User/user.types.js
@@ -2,4 +2,6 @@ export const SET_USER = "SET_USER";
 
 export const SET_USER_DATA = "SET_USER_DATA";
 
+export const SIGN_OUT = "SIGN_OUT";
+
 export const DELETE_USER = "DELETE_USER";
