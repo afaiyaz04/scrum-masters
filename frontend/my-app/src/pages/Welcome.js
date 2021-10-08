938366f402953cb4d151be377ee49577a340905a
Alex <alexandersiohsavage@gmail.com>
2021-10-08 21:32:01 +1100

    created login page

--- frontend/my-app/src/pages/Welcome.js
+++ frontend/my-app/src/pages/Welcome.js
@@ -1,10 +1,30 @@
+
+
 import { GoogleLogin } from 'react-google-login'
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router'
-import { Button } from 'antd'
+import { Carousel, Button, Layout } from 'antd'
 
 import { SET_USER } from '../redux/User/user.types'
 import { setUser } from '../redux/User/user.actions'
+import React from 'react'
+import "./Welcome.css";
+import {GoogleButton} from 'react-google-button'
+import styled from 'styled-components'
+
+
+
+
+const CarouselWrapper = styled(Carousel)`
+   > .slick-dots li button {
+       background: grey;
+  }
+  > .slick-dots li.slick-active button {
+    background: black;
+  }
+`;
+
+
 
 function SignUp() {
     const dispatch = useDispatch();
@@ -28,18 +48,62 @@ function SignUp() {
 
     const googleError = () => alert('Google Login was unsuccessful. Try again later');
 
+
+    const contentStyle = {
+        height: window.innerHeight - (64+70+10),
+        color: '#696969',
+        lineHeight: '160px',
+        textAlign: 'center',
+        background: '#fff',
+      };
+
+      const { Header, Content, Footer } = Layout;
+
+      
+
     return (
         <div>
-            <GoogleLogin
-                clientId='1063167078209-5pq5omaa5so02ga8icoeqqdk1iql3hdl.apps.googleusercontent.com'
-                render={(renderProps) => (
-                    <Button onClick={renderProps.onClick} disabled={renderProps.disabled}>Login with Google</Button>
-                )}
-                onSuccess={googleSuccess}
-                onFailure={googleError}
-                cookiePolicy='single_host_origin'
-            />
+            <Layout>
+                <Header style={{backgroundColor: "#20639B"}}>
+                    {/* <div className='logo'/> */}
+                    <div><h1 className='title'>CRM</h1></div>
+                    <div style={{float: 'right', transform: 'translate(0, 7px)'}}>
+                        <GoogleLogin
+                            clientId='1063167078209-5pq5omaa5so02ga8icoeqqdk1iql3hdl.apps.googleusercontent.com'
+                            render={(renderProps) => (
+                                <GoogleButton onClick={renderProps.onClick} disabled={renderProps.disabled}>Login with Google</GoogleButton>
+                            )}
+                            onSuccess={googleSuccess}
+                            onFailure={googleError}
+                            cookiePolicy='single_host_origin'
+                        />
+                    </div>
+                </Header>
+                
+                <Content>
+                    <div>
+                        <CarouselWrapper autoplay>
+                            <div>
+                            <h3 style={contentStyle}>Screen 1</h3>
+                            </div>
+                            <div>
+                            <h3 style={contentStyle}>Screen 2</h3>
+                            </div>
+                            <div>
+                            <h3 style={contentStyle}>Screen 3</h3>
+                            </div>
+                            <div>
+                            <h3 style={contentStyle}>Screen 4</h3>
+                            </div>
+                        </CarouselWrapper>
+                    </div>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>
+                    Created by Scrummasters
+                </Footer>
+            </Layout>
         </div>
+        
     );
 }
 
