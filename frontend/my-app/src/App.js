841f53ca3483480a8173aff98dbdf75adcd8016d
Emmanuel <emmanuel.pinca098@gmail.com>
2021-10-08 16:51:23 +1100

    modified client routes

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -8,20 +8,18 @@ import Contracts from './pages/Contracts'
 import Orders from './pages/Orders'
 import Profile from "./pages/Profile";
 import Welcome from "./pages/Welcome";
-import Auth from './pages/Auth';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/scrummasters' exact component={Welcome} />
-        <Route path='/' exact component={Dashboard} />
+        <Route path='/' exact component={Welcome} />
+        <Route path='/dashboard' exact component={Dashboard} />
         <Route path='/contacts' component={Contacts} />
         <Route path='/orders' component={Orders} />
         <Route path='/users' component={Users} />
         <Route path='/contracts' component={Contracts} />
         <Route path='/profile' component={Profile}/>
-        <Route path='/auth' component={Auth}/>
       </Switch>
     </Router>
   );
