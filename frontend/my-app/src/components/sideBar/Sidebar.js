95ba43ce5165ede94fe7794d89c5f3fed44cde39
Emmanuel <emmanuel.pinca098@gmail.com>
2021-10-15 17:09:10 +1100

    fixed formatting

--- frontend/my-app/src/components/sideBar/Sidebar.js
+++ frontend/my-app/src/components/sideBar/Sidebar.js
@@ -44,7 +44,7 @@ function Navbar() {
                                       <li key={index} className={item.cName}>
                                           <Link to={item.path}>
                                               {item.icon}
-                                              <span>{item.title}</span>
+                                              <span style={{ marginLeft: "16px" }}>{item.title}</span>
                                           </Link>
                                       </li>
                                   );
