99afcd5a9ef168ba2157c509d40d43707ca5ec32
Md Reaj Uddin Rabbi <mdreajuddinrabbi@gmail.com>
2021-10-21 10:02:57 +1100

    updated index.js (backend) for deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
-app.use(express.static(__dirname + "/"));
+app.use(express.static("/")); //__dirname
 
 app.use("/client", clientRoutes);
 app.use("/order", orderRoutes);
